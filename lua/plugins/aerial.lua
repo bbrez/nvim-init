9232fbe1ebe9d3429b11f2bdb9237609df09e3f7
Bruno de Castro Brezolin <bbrezolin@hotmail.com>
2024-05-09 13:36:41 -0300

    better theme consistency

--- lua/plugins/aerial.lua
+++ lua/plugins/aerial.lua
@@ -1,8 +1,23 @@
 return {
   'stevearc/aerial.nvim',
-  opts = {},
+  lazy = true,
+  cmd = "AerialToggle",
+  opts = {
+    layout = {
+      width = 30,
+      default_direction = 'right',
+    }
+  },
   dependencies = {
     'nvim-treesitter/nvim-treesitter',
     'nvim-tree/nvim-web-devicons'
-  }
+  },
+
+  init = function()
+    vim.keymap.set('n', '<leader>co', ':AerialToggle<CR>', {
+      noremap = true,
+      silent = true,
+      desc = 'Toggle Code Outline'
+    })
+  end,
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,5 +1,12 @@
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  opts = { options = { globalstatus = true, theme = 'catppuccin' } }
+  opts = {
+    options = {
+      globalstatus = true,
+      theme = 'catppuccin',
+      component_separators = { left = '', right = '' },
+      section_separators = { left = '', right = ''}
+    },
+  }
 }
